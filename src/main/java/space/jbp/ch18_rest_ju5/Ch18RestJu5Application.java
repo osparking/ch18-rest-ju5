3cb95fb13e66caa6201249b9251e9a430f40239e
osparking <jbpark03@gmail.com>
2024-01-11 12:32:36 +0900

    STS 4에서 starter 로 생성, 롬복

---
+++ src/main/java/space/jbp/ch18_rest_ju5/Ch18RestJu5Application.java
@@ -0,0 +1,13 @@
+package space.jbp.ch18_rest_ju5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch18RestJu5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch18RestJu5Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/jbp/ch18_rest_ju5/Ch18RestJu5ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.jbp.ch18_rest_ju5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch18RestJu5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
