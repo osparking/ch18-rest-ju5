4cb4a95d96a4d728b3ff59cdc0df6c44bbbf1ea0
osparking <jbpark03@gmail.com>
2024-01-12 21:58:12 +0900

    항공편에 승객을 더하는 부분 수정

--- src/main/java/space/jbp/ch18_rest_ju5/model/Flight.java
+++ src/main/java/space/jbp/ch18_rest_ju5/model/Flight.java
@@ -1,6 +1,7 @@
 package space.jbp.ch18_rest_ju5.model;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,11 +15,22 @@ import lombok.ToString;
 public class Flight {
   private String flightNo;
   private int seats;
-  private Collection<Passenger> passengers;
+  private Collection<Passenger> passengers = new HashSet<>();;
 
   public Flight(String flightNo, int seats) {
     super();
     this.flightNo = flightNo;
     this.seats = seats;
+  }
+
+  public boolean addPassenger(Passenger passenger) {
+    if (passengers.size() > seats) {
+      throw new RuntimeException("좌석 부족으로 탑승이 거부되었음.");
+    }
+    return passengers.add(passenger);
+  }
+  
+  public boolean removePassenger(Passenger passenger) {
+    return passengers.remove(passenger);
   }
 }
