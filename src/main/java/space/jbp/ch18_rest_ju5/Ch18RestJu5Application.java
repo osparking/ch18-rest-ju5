b6d844dd5c0797bc57733de57761d50dab2e6bd2
osparking <jbpark03@gmail.com>
2024-01-13 21:27:06 +0900

    국가 정보 맵을 JPA 를 통해 영구 저장

--- src/main/java/space/jbp/ch18_rest_ju5/Ch18RestJu5Application.java
+++ src/main/java/space/jbp/ch18_rest_ju5/Ch18RestJu5Application.java
@@ -1,13 +1,35 @@
 package space.jbp.ch18_rest_ju5;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+import space.jbp.ch18_rest_ju5.beans.FlightBuilder;
+import space.jbp.ch18_rest_ju5.model.Country;
+import space.jbp.ch18_rest_ju5.model.CountryRepository;
 
 @SpringBootApplication
+@Import(FlightBuilder.class)
 public class Ch18RestJu5Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ch18RestJu5Application.class, args);
-	}
+  @Autowired
+  private Map<String, Country> countriesMap;
 
+  public static void main(String[] args) {
+    SpringApplication.run(Ch18RestJu5Application.class, args);
+  }
+
+  @Bean
+  CommandLineRunner configureRepository(CountryRepository repository) {
+    return args -> {
+      for (Country country : countriesMap.values()) {
+        repository.save(country);
+      }
+    };
+  }
 }
