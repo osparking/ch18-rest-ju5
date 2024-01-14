cfa702123824fa8e3072fa83d3cc7f40ee17a24e
osparking <jbpark03@gmail.com>
2024-01-14 14:13:43 +0900

    승객 클래스를 개체로, ID 멤버 추가

--- src/main/java/space/jbp/ch18_rest_ju5/model/Passenger.java
+++ src/main/java/space/jbp/ch18_rest_ju5/model/Passenger.java
@@ -1,14 +1,25 @@
 package space.jbp.ch18_rest_ju5.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Setter
 @Getter
 @ToString
+@Entity
+@NoArgsConstructor
 public class Passenger {
+  @Id
+  @GeneratedValue
+  private Long id;
   private String name;
+  @ManyToOne
   private Country country;
   
   public Passenger(String name, Country country) {
