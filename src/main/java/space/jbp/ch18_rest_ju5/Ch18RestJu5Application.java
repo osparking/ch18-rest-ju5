1a16ecf75ef558e9485b1b6900ec2ed3d4b55898
osparking <jbpark03@gmail.com>
2024-01-15 15:55:27 +0900

    항공편 승객 명단 영구 저장

--- src/main/java/space/jbp/ch18_rest_ju5/Ch18RestJu5Application.java
+++ src/main/java/space/jbp/ch18_rest_ju5/Ch18RestJu5Application.java
@@ -12,6 +12,9 @@ import org.springframework.context.annotation.Import;
 import space.jbp.ch18_rest_ju5.beans.FlightBuilder;
 import space.jbp.ch18_rest_ju5.model.Country;
 import space.jbp.ch18_rest_ju5.model.CountryRepository;
+import space.jbp.ch18_rest_ju5.model.Flight;
+import space.jbp.ch18_rest_ju5.model.Passenger;
+import space.jbp.ch18_rest_ju5.model.PassengerRepository;
 
 @SpringBootApplication
 @Import(FlightBuilder.class)
@@ -20,15 +23,22 @@ public class Ch18RestJu5Application {
   @Autowired
   private Map<String, Country> countriesMap;
 
+  @Autowired
+  private Flight flight;
+
   public static void main(String[] args) {
     SpringApplication.run(Ch18RestJu5Application.class, args);
   }
 
   @Bean
-  CommandLineRunner configureRepository(CountryRepository repository) {
+  CommandLineRunner configureRepository(CountryRepository countryRepo,
+      PassengerRepository passengerRepo) {
     return args -> {
       for (Country country : countriesMap.values()) {
-        repository.save(country);
+        countryRepo.save(country);
+      }
+      for (Passenger passenger : flight.getPassengers()) {
+        passengerRepo.save(passenger);
       }
     };
   }
