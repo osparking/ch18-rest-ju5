111a0634b4462aafec0d7a640f1de52a60c11236
osparking <jbpark03@gmail.com>
2024-01-15 20:37:14 +0900

    ID 로 승객 검색 종점

--- src/main/java/space/jbp/ch18_rest_ju5/controller/PassengerController.java
+++ src/main/java/space/jbp/ch18_rest_ju5/controller/PassengerController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import space.jbp.ch18_rest_ju5.exception.NoPassengerException;
 import space.jbp.ch18_rest_ju5.model.Passenger;
 import space.jbp.ch18_rest_ju5.model.PassengerRepository;
 
@@ -17,12 +19,19 @@ import space.jbp.ch18_rest_ju5.model.PassengerRepository;
 public class PassengerController {
   @Autowired
   private PassengerRepository repository;
-  
+
   @GetMapping("/passengers")
   List<Passenger> findAll() {
     return repository.findAll();
   }
-  
+
+  @GetMapping("/passengers/{id}")
+  Passenger findById(@PathVariable Long id) {
+
+    return repository.findById(id)
+        .orElseThrow(() -> new NoPassengerException(id));
+  }
+
   @PostMapping("/passengers")
   @ResponseStatus(HttpStatus.CREATED)
   Passenger createPassenger(@RequestBody Passenger passenger) {
