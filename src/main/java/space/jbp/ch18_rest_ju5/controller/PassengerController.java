44897a7731be258782f6f0e2afcc506b6ae0a313
osparking <jbpark03@gmail.com>
2024-01-16 09:35:36 +0900

    승객 정보 갱신 종점

--- src/main/java/space/jbp/ch18_rest_ju5/controller/PassengerController.java
+++ src/main/java/space/jbp/ch18_rest_ju5/controller/PassengerController.java
@@ -1,10 +1,12 @@
 package space.jbp.ch18_rest_ju5.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import space.jbp.ch18_rest_ju5.exception.NoPassengerException;
+import space.jbp.ch18_rest_ju5.model.Country;
 import space.jbp.ch18_rest_ju5.model.Passenger;
 import space.jbp.ch18_rest_ju5.model.PassengerRepository;
 
@@ -19,6 +22,9 @@ import space.jbp.ch18_rest_ju5.model.PassengerRepository;
 public class PassengerController {
   @Autowired
   private PassengerRepository repository;
+
+  @Autowired
+  private Map<String, Country> countryMap;
 
   @GetMapping("/passengers")
   List<Passenger> findAll() {
@@ -36,5 +42,30 @@ public class PassengerController {
   @ResponseStatus(HttpStatus.CREATED)
   Passenger createPassenger(@RequestBody Passenger passenger) {
     return repository.save(passenger);
+  }
+
+  @PatchMapping("/passengers/{id}")
+  Passenger updatePassenger(@RequestBody Map<String, String> updates,
+      @PathVariable Long id) {
+    return repository.findById(id).map(passenger -> {
+      String name = updates.get("name");
+      if (name != null) {
+        passenger.setName(name);
+      }
+
+      String registered = updates.get("registered");
+      if (registered != null) {
+        passenger
+            .setRegistered(registered.equalsIgnoreCase("true") ? true : false);
+      }
+
+      Country newCountry = countryMap.get(updates.get("country"));
+      if (newCountry != null) {
+        passenger.setCountry(newCountry);
+      }
+
+      return repository.save(passenger);
+
+    }).orElseThrow(() -> new NoPassengerException(id));
   }
 }
