5f50f79cb196830f42f2caa0cda202891c844051
osparking <jbpark03@gmail.com>
2024-01-17 14:58:13 +0900

    바른 정적 메소드를 수입, 예외 투척 포함

--- src/test/java/space/jbp/ch18_rest_ju5/controller/RestAppTest.java
+++ src/test/java/space/jbp/ch18_rest_ju5/controller/RestAppTest.java
@@ -2,8 +2,8 @@ package space.jbp.ch18_rest_ju5.controller;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -15,11 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import space.jbp.ch18_rest_ju5.beans.FlightBuilder;
 import space.jbp.ch18_rest_ju5.model.Country;
 import space.jbp.ch18_rest_ju5.model.CountryRepository;
 
@@ -37,7 +35,7 @@ class RestAppTest {
   private CountryRepository countryRepo;
 
   @Test
-  void testGetCountries() {
+  void testGetCountries() throws Exception {
     when(countryRepo.findAll())
         .thenReturn(new ArrayList<>(countryMap.values()));
     mvc.perform(get("/countries"))
